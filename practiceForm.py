0ccf4ae2628a543559822d38d5c0208e50edd244
SUPS0110 <suprima0110@gmail.com>
2025-04-20 00:13:09 +0545

    subjectFieldTest1

--- practiceForm.py
+++ practiceForm.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import  expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 driver=webdriver.Edge()
 driver.maximize_window()
 driver.get("https://demoqa.com/automation-practice-form")
