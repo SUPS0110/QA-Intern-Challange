abc235a602609581989042c14ab6001de9640411
SUPS0110 <suprima0110@gmail.com>
2025-04-20 00:41:41 +0545

    formSubmit1

--- practiceForm.py
+++ practiceForm.py
@@ -1,60 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import  expected_conditions as EC
-from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 driver=webdriver.Edge()
 driver.maximize_window()
 driver.get("https://demoqa.com/automation-practice-form")
 wait=WebDriverWait(driver,10)
-#nameField
-firstName=driver.find_element(By.XPATH,"//input[@id='firstName']")
-firstName.send_keys("abc")
-lastName=driver.find_element(By.XPATH,"//input[@id='lastName']")
-lastName.send_keys("xyz")
-#emailField
-email=driver.find_element(By.XPATH,"//input[@id='userEmail']")
-email.send_keys("abc@gmail.com")
-#genderField
-gender=driver.find_element(By.XPATH,"//label[text()='Female']")
-gender.click()
-#mobileField
-mobile=driver.find_element(By.XPATH,"//input[@id='userNumber']")
-mobile.send_keys("9876543210")
-#DOBField
-dob=wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='dateOfBirthInput']")))
-dob.click()
-dob_year=Select(driver.find_element(By.CLASS_NAME,"react-datepicker__year-select"))
-dob_year.select_by_visible_text("2025")
-dob_month=Select(driver.find_element(By.CLASS_NAME,"react-datepicker__month-select"))
-dob_month.select_by_value("3")
-dob_date=driver.find_element(By.XPATH,"//div[@class='react-datepicker__day react-datepicker__day--011 react-datepicker__day--weekend']")
-dob_date.click()
-#subjectField
-subject = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='subjectsInput']")))
-subject.send_keys("!@#$%")
-subject.send_keys(Keys.TAB)
-#hobbiesField
-hobby=driver.find_element(By.XPATH,"//label[text()='Reading']")
-hobby.click()
-#pictureField
-picture=driver.find_element(By.XPATH,"//input[@id='uploadPicture']")
-picture.send_keys(r"C:\Users\asus\Downloads\OIP.jpg")
-#addressField
-address=driver.find_element(By.XPATH,"//textarea[@id='currentAddress']")
-address.send_keys("KTM")
-#state&cityField
-dropdownState=wait.until(EC.element_to_be_clickable((By.XPATH,"//div[@id='state']")))
-driver.execute_script("arguments[0].scrollIntoView(true)",dropdownState)
-dropdownState.click()
-optionState=wait.until(EC.element_to_be_clickable((By.XPATH,"//div[text()='NCR']")))
-optionState.click()
-dropdownCity=wait.until(EC.element_to_be_clickable((By.XPATH,"//div[@id='city']")))
-dropdownCity.click()
-optionCity=wait.until(EC.element_to_be_clickable((By.XPATH,"//div[text()='Noida']")))
-optionCity.click()
-
 #submitField
 submitBtn=driver.find_element(By.XPATH,"//button[text()='Submit']")
 submitBtn.submit()
