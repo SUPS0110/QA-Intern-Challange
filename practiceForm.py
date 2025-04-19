de5a8426a4fe4ec444565b1ce06f6f1291a309c8
SUPS0110 <suprima0110@gmail.com>
2025-04-20 00:14:14 +0545

    subjectFieldTest2

--- practiceForm.py
+++ practiceForm.py
@@ -33,8 +33,7 @@ dob_date=driver.find_element(By.XPATH,"//div[@class='react-datepicker__day react
 dob_date.click()
 #subjectField
 subject = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='subjectsInput']")))
-subject.send_keys("a")
-subject.send_keys(Keys.TAB)
+subject.send_keys("")
 #submitField
 submitBtn=driver.find_element(By.XPATH,"//button[text()='Submit']")
 submitBtn.submit()
